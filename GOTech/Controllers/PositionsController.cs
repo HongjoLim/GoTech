9ff65332fdb46f8e07adf03de157dc26f9775ee4
Jo Lim <hongjo1988@gmail.com>
2019-03-26 18:09:02 -0400

    Finish Scaffolding Project & Position entities
    Controllers and Views with basic CRUD functions have been created by scaffolding

---
+++ GOTech/Controllers/PositionsController.cs
@@ -0,0 +1,131 @@
+﻿using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+using GOTech.Models;
+
+/* 
+ * Name: Jo Lim
+ * Date: Mar 26, 2019
+ * Last Modified: Mar 25, 2019
+ * Description: This controller provides CRUD function to Position entity
+ *                   This controller will ONLY be accessible to Administrator
+ * */
+
+namespace GOTech.Controllers
+{
+    public class PositionsController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Positions
+        public ActionResult Index()
+        {
+            return View(db.Positions.ToList());
+        }
+
+        // GET: Positions/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Position position = db.Positions.Find(id);
+            if (position == null)
+            {
+                return HttpNotFound();
+            }
+            return View(position);
+        }
+
+        // GET: Positions/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Positions/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "PositionId,Title")] Position position)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Positions.Add(position);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(position);
+        }
+
+        // GET: Positions/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Position position = db.Positions.Find(id);
+            if (position == null)
+            {
+                return HttpNotFound();
+            }
+            return View(position);
+        }
+
+        // POST: Positions/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "PositionId,Title")] Position position)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(position).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(position);
+        }
+
+        // GET: Positions/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Position position = db.Positions.Find(id);
+            if (position == null)
+            {
+                return HttpNotFound();
+            }
+            return View(position);
+        }
+
+        // POST: Positions/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Position position = db.Positions.Find(id);
+            db.Positions.Remove(position);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
